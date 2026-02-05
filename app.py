91cc9cd9794eb070cbbe184fdd972fd9f8d151a3
JJ Chabut <jjchabut@gmail.com>
2026-02-05 20:09:58 +0100

    initial

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from vega_datasets import data
+import matplotlib.pyplot as plt
+
+source = data.cars()
+
+st.header("Visualization")
+
+st.subheader("Matplotlib")
+
+plt.figure(figsize=(12,8))
+plt.scatter(source['Horsepower'], source['Miles_per_Gallon'])
+st.pyplot(plt)
